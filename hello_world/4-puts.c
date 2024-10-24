f79cfdbb3de7e06f1c53c2a2f5aba5b2ad705a5f
ibrahimnurii <ibrakhimnuri@gmail.com>
2024-10-24 05:13:37 +0000

    puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+const char *message = "\"Programming is like building a multilingual puzzle";
+puts(message);
+return (0);
+}
